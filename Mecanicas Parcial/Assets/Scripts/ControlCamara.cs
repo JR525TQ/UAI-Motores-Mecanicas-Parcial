0dc6182a940837e68387940835ddb5d61c807390
Francisco Chappani <33500754+JR525TQ@users.noreply.github.com>
2020-05-18 17:52:24 -0300

    save

---
+++ Mecanicas Parcial/Assets/Scripts/ControlCamara.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ControlCamara : MonoBehaviour
+{
+
+    public float speed = 20.0f;
+    public float borde = 15.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.mousePosition.y >= Screen.height - borde)
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= borde)
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x >= Screen.width - borde)
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x <= borde)
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
--- Mecanicas Parcial/Assets/Scripts/ControlJugador.cs
+++ Mecanicas Parcial/Assets/Scripts/ControlJugador.cs
@@ -7,27 +7,31 @@ using UnityEngine.AI;
 public class ControlJugador : MonoBehaviour
 {
 
- // private NavMeshAgent navMeshAgent;
+  //private NavMeshAgent navMeshAgent;
     private CapsuleCollider capsuleCollider;
     private Rigidbody rb;
-    public LayerMask capaPiso;
+    private Animator animator;
     public Camera cam;
+    public LayerMask capaPiso;
     public float magnitudSalto;
+    public float rapidez;
 
     // Start is called before the first frame update
     void Start()
     {
-     // navMeshAgent = GetComponent<NavMeshAgent>();
+      //navMeshAgent = GetComponent<NavMeshAgent>();
         capsuleCollider = GetComponent<CapsuleCollider>();
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray rayo = cam.ScreenPointToRay(Input.mousePosition);
+        /*Ray rayo = cam.ScreenPointToRay(Input.mousePosition);
+        ClickToMove(rayo);*/
 
-        ClickToMove(rayo);
+        MovimientoPlayer();
 
         Salto();
     }
@@ -37,21 +41,48 @@ public class ControlJugador : MonoBehaviour
         if(other.gameObject.CompareTag("Coleccionable"))
         {
             /*vMeshAgent.acceleration += 20;
-            navMeshAgent.speed += 20;
+            navMeshAgent.speed += 20;*/
+            rapidez += 1.5f;
             other.gameObject.SetActive(false);
-            Destroy(other.gameObject);*/
+            Destroy(other.gameObject);
         }
     }
 
     void Salto()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && EstaEnElPiso())
         {
             rb.AddForce(Vector3.up * magnitudSalto, ForceMode.Impulse);
         }
     }
 
-    void ClickToMove(Ray rayo)
+    void MovimientoPlayer()
+    {
+        Vector3 direcciones = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        Vector3 movimiento = cam.transform.InverseTransformDirection(direcciones);
+        movimiento.y = 0.0f;
+        movimiento.Normalize();
+
+        if(movimiento != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(movimiento), Time.deltaTime * 7.3f);
+            animator.SetBool("estaCorriendo", true);
+        } else {
+            animator.SetBool("estaCorriendo", false);
+        }
+        
+
+        transform.Translate(movimiento * rapidez * Time.deltaTime, Space.World);
+    }
+
+    private bool EstaEnElPiso()
+    {
+        return Physics.CheckCapsule(capsuleCollider.bounds.center, new Vector3(capsuleCollider.bounds.center.x, capsuleCollider.bounds.min.y,
+                            capsuleCollider.bounds.center.z), capsuleCollider.radius* .9f, capaPiso);
+    }
+
+       //No encontre manera de saltar con la barra espaciadora y mover el personaje con navmesh al mismo tiempo.
+/*   void ClickToMove(Ray rayo)
     {
         RaycastHit hit;
 
@@ -59,10 +90,12 @@ public class ControlJugador : MonoBehaviour
         {
             if (Physics.Raycast(rayo, out hit))
                 {
-                // navMeshAgent.destination = hit.point;
+                    navMeshAgent.destination = hit.point;
                 }            
         }
     }
+*/
+
 }
 
 
