54488152174ad2455e7851aeaf0cb848aa0e8117
Francisco Chappani <33500754+JR525TQ@users.noreply.github.com>
2020-05-18 23:37:28 -0300

    save dia 2

---
+++ Mecanicas Parcial/Assets/Scripts/ControlTorreta.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTorreta : MonoBehaviour
+{
+    public float speedDisparo;
+    public float damage = 20.0f;
+    public float rango = 15.0f;
+
+    private Vector3 target;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void UpdateTarget()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, rango);
+    }
+}
